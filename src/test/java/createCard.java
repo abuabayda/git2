975c5612fd4df48f9d0aa9a5f148312983d3d543
abuabayda <58151959+abuabayda@users.noreply.github.com>
2020-02-29 15:34:21 -0500

    Revert "update createCard"

--- src/test/java/createCard.java
+++ src/test/java/createCard.java
@@ -5,13 +5,8 @@ public class createCard {
         System.out.println(" just practicing gitHub");
 
 
-        System.out.println("want to check confilict");
-
-
-
 
         //this line is from Asiya
-
 
 
     }
